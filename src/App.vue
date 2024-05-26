7bdf8c3aed5cefb1fce232dbbd4b7121655f467b
nlqs <nlqs@nlqs.com>
2024-05-26 10:49:11 +0800

    调整路由结构

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@
  * @Description: 
 -->
 <template>
-  <router-view>
-
-  </router-view>
+  <router-view></router-view>
 </template>
 
 <style lang="less">
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -4,11 +4,16 @@
  * @Description: 
 -->
 <script setup>
+import LayoutHeader from '@/views/layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/layout/components/LayoutFooter.vue'
+
 </script>
 
 <template>
+  <LayoutHeader></LayoutHeader>
   <!-- 二级路由出口 -->
   <router-view></router-view>
+  <LayoutFooter></LayoutFooter>
 </template>
 
 <style scoped>
