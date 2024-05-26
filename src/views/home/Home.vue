32286e3735e81d9a43a2e748a506a50145a77e45
NanluQingshi <yimo@mail.ustc.edu.cn>
2024-05-26 10:49:11 +0800

    调整路由结构

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,15 +4,11 @@
  * @Description: 
 -->
 <script setup>
-import LayoutHeader from '@/views/layout/components/LayoutHeader.vue'
-import LayoutFooter from '@/views/layout/components/LayoutFooter.vue'
 
 </script>
 
 <template>
-  <LayoutHeader></LayoutHeader>
   <div>首页</div>
-  <LayoutFooter></LayoutFooter>
 </template>
 
 <style scoped lang="less">
