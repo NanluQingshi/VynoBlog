135648b0fdd2d7f6597cb2b2dd0a004e839f1641
NanluQingshi <yimo@mail.ustc.edu.cn>
2024-05-26 10:45:42 +0800

    页面底部组件结构

---
+++ src/views/layout/components/LayoutFooter.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+
+<template>
+  <div class="footer">
+    <img src="@/assets/logo.png" alt="">
+    <span>Vynon博客</span>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 6vw;
+  background-color: #333;
+  cursor: default;
+
+  img {
+    width: 3vw;
+    height: 3vw;
+  }
+
+  span {
+    margin-left: 1vw;
+    color: #fff;
+    line-height: 3vw;
+  }
+}
+</style>
