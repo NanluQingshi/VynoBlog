4883d589148403b683641fb266081483ec52db72
NanluQingshi <yimo@mail.ustc.edu.cn>
2024-05-26 17:05:11 +0800

    登录页面结构

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,9 +1,71 @@
+<!--
+ * @Author: nlqs
+ * @Date: 2024-05-25 19:26:05
+ * @Description: 
+-->
 <script setup>
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <div>登录</div>
+  <div class="box">
+    <div class="register">
+      <p>没有账号?</p>
+      <p>点击此处去注册</p>
+      <p class="register-btn" @click="$router.push('/register')">注册</p>
+    </div>
+    <div class="login">
+      <Main op="Login"></Main>
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
+.box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 67.5rem;
+  position: absolute;
+  // 以左上角与父元素为基准
+  left: 50%;
+  top: 50%;
+  // 以自身中心为基准移动
+  transform: translate(-50%, -50%);
+
+  .register {
+    padding: 2rem 4rem;
+    width: 30.5rem;
+    height: 13.5rem;
+    background-color: #333333;
+    color: #fff;
+    font-size: 1.3rem;
+
+    p {
+      margin-bottom: 1.5rem;
+    }
+
+    .register-btn {
+      text-align: center;
+      width: 6rem;
+      border: 1px solid #4cc9f0;
+      cursor: pointer;
+      background-color: transparent;
+      color: #4cc9f0;
+
+      &:hover {
+        color: #fff;
+        background-color: #4cc9f0;
+      }
+    }
+  }
+
+  .login {
+    flex: 1;
+    height: 30rem;
+    background-color: #fff;
+
+    box-shadow: 0 0 10px 10px rgba(51, 51, 51, 0.37)
+  }
+}
 </style>
---
+++ src/views/user/components/Main.vue
@@ -0,0 +1,98 @@
+<!--
+ * @Author: nlqs
+ * @Date: 2024-05-26 15:48:16
+ * @Description: 登录注册页通用组件
+-->
+<script setup>
+import { ref, defineProps } from 'vue'
+
+// 用户名
+const username = ref('')
+// 密码
+const password = ref('')
+
+// 接收父组件传递的参数
+defineProps({
+  op: String
+}) 
+
+</script>
+
+<template>
+  <div class="side-box">
+    <div class="title">
+      <p class="name">{{ op }} - Vynon Blog</p>
+      <p>Talk is cheap, Show me the code</p>
+    </div>
+    <div class="form">
+      <input 
+        type="text" 
+        placeholder="请输入用户名"
+        v-model="username"
+      >
+      <input 
+        type="password"
+        placeholder="请输入密码"
+        v-model="password" 
+      >
+      <button @click="goLogin">{{ op }}</button>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.side-box {
+  padding: 1.5rem 3.8rem;
+
+
+  .title {
+    text-align: center;
+    height: 6rem;
+    font-size: 1.6rem;
+    color: #7e7e7e;
+
+    .name {
+      margin-bottom: 1.5rem;
+      color: #333;
+      font-size: 2rem;
+    }
+  }
+
+  .form {
+    height: 20rem;
+    background-color: #fff;
+    text-align: center;
+
+    input {
+      margin-bottom: 3.2rem;
+      padding-left: 1.2rem;
+      width: 20.75rem;
+      height: 3.2rem;
+      font-size: 1.1rem;
+      border: 1px solid #333;
+      border-radius: 1.3rem;
+      outline: nine;
+      transition: 0.8s;
+
+      &:nth-child(1) {
+        margin-top: 3.2rem;
+      }
+
+      &:focus {
+        font-size: 1.5rem;
+        box-shadow: 0 0 5px 5px rgba(129, 146, 202, 0.356);
+      }
+    }
+
+    button {
+      margin: 0 auto;
+      display: block;
+      width: 20.75rem;
+      height: 2.5rem;
+      font-size: 1rem;
+      cursor: pointer;
+    }
+  }
+}
+
+</style>
