53e230fb25eeba168ca6b22917a31e5d23770da7
NanluQingshi <yimo@mail.ustc.edu.cn>
2024-05-26 10:27:36 +0800

    封装搜索框组件-页面结构和样式

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,65 @@
 <!--
  * @Author: nlqs
  * @Date: 2024-05-25 22:21:50
- * @Description: 
+ * @Description: 搜索框组件
 -->
 <script setup>
+import { ref } from 'vue'
+
+// 搜索内容
+const content = ref('')
 </script>
 
 <template>
-  <div>搜索框</div>
+  <div class="search">
+    <input
+      type="text"
+      placeholder="请输入搜索内容"
+      v-model.trim="content"
+    >
+
+    <div class="img">
+      <img src="@/assets/search.png" alt="" @click="doSearch">
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
-div {
+.search {
+  position: relative;
+  display: flex;
+  align-items: center;
   width: 26vw;
   height: 100%;
+
+  input {
+    padding-left: 0.8vw;
+    width: 100%;
+    height: 5vh;
+    // 移除元素的默认外边框
+    outline: none;
+    border: none;
+    border-radius: 1vw 0 0 1vw;
+    transition: .6s;
+
+    &:focus {
+      font-size: 1vw;
+      box-shadow: 0 0 5px 5px rgba(200, 233, 224, 0.34);
+    }
+  }
+
+  .img {
+    position: absolute;
+    right: 0;
+    height: 5vh;
+    width: 5vh;
+    background-color: #ccc;
+    cursor: pointer;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
