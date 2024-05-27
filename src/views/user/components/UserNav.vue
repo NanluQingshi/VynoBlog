ea618e8fe9b719d431c1589f4d7b4beb9f200ef3
nlqs <nlqs@nlqs.com>
2024-05-27 21:52:58 +0800

    封装用户导航栏组件

--- src/views/user/components/UserNav.vue
+++ src/views/user/components/UserNav.vue
@@ -7,13 +7,45 @@
 </script>
 
 <template>
-  <div>导航栏</div>
+  <div class="nav">
+    <ul>
+      <li><a href="javascript:void(0)" @click="$router.push('/user')">个人资料</a></li>
+      <li><a href="javascript:void(0)" @click="$router.push('/user/publish')">编辑博客</a></li>
+      <li><a href="javascript:void(0)" @click="$router.push('/user/like')">我的点赞</a></li>
+      <li><a href="javascript:void(0)" @click="$router.push('/user/collect')">我的收藏</a></li>
+      <li><a href="javascript:void(0)" @click="$router.push('/user/blog')">我的博客</a></li>
+    </ul>
+  </div>
 </template>
 
 <style lang="less" scoped>
-div {
-  width: 100px;
-  height: 100px;
-  background-color: pink;
+.nav {
+  width: 15rem;
+  height: 100%;
+  background-color: #fff;
+
+  ul {
+    height: 100%;
+
+    li {
+      width: 15rem;
+      height: 4rem;
+
+      a {
+        display: block;
+        width: 15rem;
+        height: 4rem;
+        text-align: center;
+        line-height: 4rem;
+        font-size: 1.2rem;
+
+        &:hover {
+          color: #fff;
+          background-color: #000;
+          box-shadow: 0 0 5px 5px rgba(200, 233, 224, 0.34);
+        }
+      }
+    }
+  }
 }
 </style>
