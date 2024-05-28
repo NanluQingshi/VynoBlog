1e4a4b5b01795aaa2ad749b9f2f010b322b8fe93
nlqs <nlqs@nlqs.com>
2024-05-28 11:26:38 +0800

    实现功能-获取用户信息

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,9 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 配置热更新
+  devServer: {
+    hot: true,
+    liveReload: true
+  }
 })
